af2f445726ffb361cfc5959419c0ee8a15f23b95
Matheus <matheusmlp3@gmail.com>
2024-09-17 01:01:49 -0300

    primeiro commit

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import '../css/HeroSection.css';
+import wallpaper from '../assets/wallpaper.jpg'; 
+
+const HeroSection: React.FC = () => {
+  return (
+    <div>
+      <section id="home">
+        <div className='hero-section'>
+          <div className="overlay bg-dark"/>
+          
+          <div 
+            className="background-image parallax"
+            style={{ backgroundImage: `url(${wallpaper})` }} 
+          />
+          <div className="content-container section fullscreen d-flex">
+            <div className="container align-self-center">
+              <div className="row">
+                <div className="col text-center">
+                  <div className="dynamic-text">
+                    <h1>Desenvolvedor de Software</h1>
+                  </div>
+                  <p className="welcome-text">Bem vindo</p>
+                  <a href="#contato" className="btn-contato">Contato</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/Sobre.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/Sobre.css';
+
+
+const Sobre: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Sobre
